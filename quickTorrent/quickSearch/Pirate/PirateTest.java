d48c1276413ca83d2659e5b74c22c9b4ea137c48
Jamin Becker <jaminbecker@gmail.com>
2013-05-28 13:13:20 -0400

    Fixed some merging conflicts

--- quickTorrent/quickSearch/Pirate/PirateTest.java
+++ quickTorrent/quickSearch/Pirate/PirateTest.java
@@ -1,9 +1,11 @@
 package quickSearch.Pirate;
 
 import quickSearch.Pirate.PirateSimpleSearch;
+import quickSearch.Pirate.PirateSimpleSearch;
 
 public class PirateTest {
 	public static void main(String[] args) {
+		PirateSimpleSearch mySearch = new PirateSimpleSearch("Linkin Park In the End", true);
 		System.out.println(mySearch.FindBestDownload());
 
 	}
