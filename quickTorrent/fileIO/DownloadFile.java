4ae48ea7ea8f4dcbbd142d5073b2bc32dac9b6ee
Jamin Becker <jaminbecker@gmail.com>
2013-05-28 14:38:11 -0400

    Parameters passed to through the constructor rather than directly to the DownloadFile Method

--- quickTorrent/fileIO/DownloadFile.java
+++ quickTorrent/fileIO/DownloadFile.java
@@ -9,28 +9,34 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DownloadFile {
-	public void GetFileDownload(String URI, String hash) throws IOException{ 
+	String name = "temp";
+	String localStorePath = "";
+	String downloadURI;
+	
+	public DownloadFile(String downloadURI, String name){
+		this.name = name;
+		this.downloadURI = downloadURI;
+	}
+	public void GetFileDownload() throws IOException{ 
 		/*
 		 * Given a URI this method will grab the binary data from the server using http protocol
 		 */
 		URL inputURI;
 		HttpURLConnection connect;
-			inputURI = new URL(URI);
+			inputURI = new URL(this.downloadURI);
 			connect = (HttpURLConnection) inputURI.openConnection();
 		    connect.setReadTimeout(10000);
 			connect.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.3) Gecko/20100401");
 			connect.setRequestMethod("GET");
-			
-			String fileName = hash + ".torrent";
-		 
-		     HttpURLConnection http = (HttpURLConnection)inputURI.openConnection();
-		     InputStream input  = http.getInputStream();
-		     byte[] buffer = new byte[2048];
-		     int n = -1;
-		     OutputStream output = new FileOutputStream( new File( fileName ));
-		     while ((n = input.read(buffer)) != -1) { 
-		        output.write( buffer, 0, n );
-		     }
-		     output.close();
+			String fileName = name + ".torrent";
+		    HttpURLConnection http = (HttpURLConnection)inputURI.openConnection();
+		    InputStream input  = http.getInputStream();
+		    byte[] buffer = new byte[2048];
+		    int n = -1;
+		    OutputStream output = new FileOutputStream( new File( fileName ));
+		    while ((n = input.read(buffer)) != -1) { 
+		       output.write( buffer, 0, n );
+		    }
+		    output.close();
 	}
 }
--- quickTorrent/fileIO/DownloadTest.java
+++ quickTorrent/fileIO/DownloadTest.java
@@ -5,9 +5,8 @@ import java.io.IOException;
 public class DownloadTest {
 
 	public static void main(String[] args) throws IOException {
-		DownloadFile myTorrentLink = new DownloadFile();
-		System.out.println("http://isohunt.com/download/161705833/05402962822e5db9c1b9f613d7f139b14a7d818b.torrent");
-		myTorrentLink.GetFileDownload("http://isohunt.com/download/161705833/05402962822e5db9c1b9f613d7f139b14a7d818b.torrent", "test");
+		DownloadFile myTorrentLink = new DownloadFile("http://isohunt.com/download/161705833/05402962822e5db9c1b9f613d7f139b14a7d818b.torrent", "c:\\users\\fjbecker\\Desktop\\One-Metallica");
+		myTorrentLink.GetFileDownload();
 
 	}
 
