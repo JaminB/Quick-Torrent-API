f4f33377666e52871d269d77d152c53577b3484e
Jamin Becker <jaminbecker@gmail.com>
2013-06-25 22:45:23 -0400

    added globals package

---
+++ quickTorrent/globals/Variables.java
@@ -0,0 +1,24 @@
+package globals;
+
+public class Variables {
+	
+	public static String currentURI; //URI currently being searched
+	public static String currentWebPage; //Webpage currently being searched
+	
+	public static String magnetLink; //Most recent magnetLink
+	public static String torrentPreview; //Preview of content of most recent .torrent file
+	
+	public static String downloadUri; //URI location of most recent torrent download
+	public static String downloadName; //Name of File and download location.
+	
+	public static String query; //Most recent search query
+	public static String mediaType; //Movie/Music/TV/Other (movie and music are currently the only available fields)
+	public static boolean qualityCheck; //Safe-search enabled
+	
+	
+	
+
+	
+	
+	
+}
