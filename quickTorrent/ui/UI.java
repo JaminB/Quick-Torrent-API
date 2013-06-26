c76368dbed23515ee829feeeeac3a0e6ef152019
ecamarata <ecamarata@Mustang.amac.local>
2013-06-26 11:34:28 -0400

    added UI class

---
+++ quickTorrent/ui/UI.java
@@ -0,0 +1,15 @@
+/*
+ Ecamarat   
+ */
+package ui;
+
+
+public class UI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+}
