073e734d201920e8d5436de4365d9f72565369c8
Jamin Becker <jaminbecker@gmail.com>
2013-08-21 00:20:51 -0400

    More work on heuristic

--- quickTorrent/rating/RateTrackers.java
+++ quickTorrent/rating/RateTrackers.java
@@ -1,4 +1,7 @@
 package rating;
+import java.util.LinkedList;
+import java.util.List;
+
 import cache.*;
 
 public class RateTrackers {
@@ -8,7 +11,6 @@ public class RateTrackers {
 	int h = 0; //heuristic used to find the best link
 	int g = 100; //goal state
 	double linkDetailLevel = 0;
-	
 	public void setLinkDetailLevel(String searchTerm){
 		int index = searchTerm.indexOf(" ");
 		float count = 0;
@@ -22,8 +24,52 @@ public class RateTrackers {
 		System.out.println(linkDetailLevel);
 	}
 	
-	public void setSearchLinkAccuracy(String searchTerm, String detailPage){
+	private boolean insignificantWord(String someWord){
+		String[] ignoreList = {
+				"a","an","the",
+				"and", "but", "for", "nor", "or", "yet", 
+				"am", "is", "are", "was","were", "be", "being", "is", 
+				"have", "has","had","do","does","did", "shell", "will", "should","would","may","might","must","can", "could",
+				"about", "above", "across", "after", "against", "around", "at", "before", "behind", "below", "beneath", "beside",
+				"besides", "between", "beyond", "by", "down", "during", "except", "for", "from", "in","inside","into","like","near","of",
+				"off","on","out","outside","over","since","through","throughout","till","to","toward","under","until","up","upon","with",
+				"without"
+				};
+		for(int i = 0; i < ignoreList.length; i++)
+			if(someWord.equals(ignoreList[i]))
+					return true;
+		return false;
 		
+	}
+	public void setSearchLinkAccuracy(String searchTerm, String detailPage){
+		//List<String> searchTerms = new LinkedList<String>(); // create a new list
+		int insignificantWordCount = 0;
+		int significantWordCount = 0;
+		int startIndex = 0;
+		int endIndex = 0;
+		for (int i = 0; i < searchTerm.length(); i++){
+			endIndex++;
+			if(searchTerm.charAt(i) == ' '){
+				String untestedWord = (searchTerm.substring(startIndex, endIndex).replaceAll("\\s", "").toLowerCase());
+				if(insignificantWord(untestedWord)){
+					insignificantWordCount++;
+				}
+					//searchTerms.add(untestedWord);
+				else
+					significantWordCount++;
+				startIndex = endIndex;
+			}
+		}
+		String untestedWord = (searchTerm.substring(startIndex, searchTerm.length()).replaceAll("\\s", "").toLowerCase());
+		if(insignificantWord(untestedWord)){
+			insignificantWordCount++;
+		}
+			//searchTerms.add(untestedWord);
+		else
+			significantWordCount++;
+		
+		System.out.println("Insignificant words: " + insignificantWordCount + "" +"\nSignificant words: "+ significantWordCount );
+			
 	}
 
 
--- quickTorrent/tests/GenericRatingTest.java
+++ quickTorrent/tests/GenericRatingTest.java
@@ -6,7 +6,9 @@ public class GenericRatingTest {
 	public static void main(String[] args) {
 		RateTrackers rating = new RateTrackers();
 		rating.setLinkDetailLevel("Test space allocation");
-
+		rating.setSearchLinkAccuracy("In the end linkin park", "whatever");
 	}
+	
+	
 
 }
